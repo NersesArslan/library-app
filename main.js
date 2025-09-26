56f2b4736acf7a9b7557bd4de0a1675508ef239d
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2025-09-25 21:02:43 -0700

    added button that shows form, form adds new values to library

--- main.js
+++ main.js
@@ -15,21 +15,40 @@ addBookToLibrary("Madame Bovary", "Gustave Flaubert");
 addBookToLibrary("Ulysses", "James Joyce");
 addBookToLibrary("The Autobiography of Malcom X", "Alex Hailey");
 
-let Library = [
-  "To Kill a Mockingbirdm",
-  "The Devil Wears Prada",
-  "Harry Potter and the Flight",
-];
-
 const output = document.getElementById("output");
 
 function displayLibrary(array) {
   output.innerHTML = "";
   array.forEach((book) => {
     const newBookElemenmt = document.createElement("div");
-    newBookElemenmt.textContent = book.title;
+    newBookElemenmt.classList.add("book");
+    newBookElemenmt.textContent = `${book.title} by ${book.author}`;
+
     output.appendChild(newBookElemenmt);
   });
 }
+
+const showFormButton = document.getElementById("showFormButton");
+const myForm = document.getElementById("myForm");
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(myForm);
+  const data = Object.fromEntries(formData.entries());
+
+  addBookToLibrary(data.title, data.author);
+  displayLibrary(myLibrary);
+});
+
+showFormButton.addEventListener("click", () => {
+  if (myForm.style.display === "none") {
+    myForm.style.display = "block";
+    showFormButton.innerHTML = "Close Form";
+  } else {
+    myForm.style.display = "none";
+    showFormButton.innerHTML = "Show Form";
+  }
+});
 
 displayLibrary(myLibrary);
